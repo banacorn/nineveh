558c77c5e470baf772a7c49a2585e5a5248c41ee
Ting-Yen LAI <banacorn@gmail.com>
2013-01-07 03:12:11 +0800

    i munch args now

--- main.hs
+++ main.hs
@@ -1,1 +1,34 @@
-main = putStrLn "hello haskell"
+module Main where
+
+--import Text.Parsec
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Language (emptyDef)
+import qualified Text.ParserCombinators.Parsec.Token as T
+import System.Environment (getArgs)
+import Data.List
+
+--lexer :: T.TokenParser ()
+--lexer  = T.makeTokenParser emptyDef
+
+--whiteSpace  = T.whiteSpace lexer
+--lexeme      = T.lexeme lexer
+--symbol      = T.symbol lexer
+--natural     = T.natural lexer
+--parens      = T.parens lexer
+--semi        = T.semi lexer
+
+type Path = String
+type Arguments = (String, String)
+
+munchArgs :: IO Arguments
+munchArgs = do
+    args <- getArgs
+    case args of
+        [source, target] -> 
+            return (source, target)
+        otherwise ->
+            error "less2stylus [-bsc] <source> <target>"
+
+main = do
+    arguments <- munchArgs
+    putStrLn . show $ arguments
