6acfdd3b6b2e5471ff691d0b22e2ac38f88348e2
Ting-Yen LAI <banacorn@gmail.com>
2013-01-23 13:19:27 +0800

    hub for all values

--- less.hs
+++ less.hs
@@ -5,21 +5,7 @@ import Data.List
 
 import Text.ParserCombinators.Parsec
 import Parser
-import Value.Color
-import Value.Number
-import Value.Integer
-import Value.Percentage
-import Value.Length
-import Value.Position
-import Value.Ratio
-import Value.Frequency
-import Value.Resolution
-import Value.Shape
-import Value.Angle
-import Value.String
-import Value.Url
-import Value.Time
-import Value.TimingFunction
+import Value.Value
 
 
 
---
+++ value/value.hs
@@ -0,0 +1,17 @@
+module Value.Value where
+
+import Value.Color
+import Value.Number
+import Value.Integer
+import Value.Percentage
+import Value.Length
+import Value.Position
+import Value.Ratio
+import Value.Frequency
+import Value.Resolution
+import Value.Shape
+import Value.Angle
+import Value.String
+import Value.Url
+import Value.Time
+import Value.TimingFunction
